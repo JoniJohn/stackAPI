8d47750bb1b9544b7b3dab8cbebcc815581d121b
Lesley Thuto Johnson <whosekid@gmail.com>
2022-01-27 13:01:05 +0200

    Adding Template

--- web/api_services/posts.py
+++ web/api_services/posts.py
@@ -1,5 +1,5 @@
 import requests
 
-def get_post():
+def get_posts():
     res=requests.get('https://api.stackexchange.com/2.3/posts?order=desc&sort=activity&site=stackoverflow').json()
     return res['items']
