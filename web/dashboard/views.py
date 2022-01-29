38a6062dff21c8a41be777d58a6672a4ca9097b7
Lesley Thuto Johnson <whosekid@gmail.com>
2022-01-29 02:42:40 +0200

    Fix to slow UI loading

--- web/dashboard/views.py
+++ web/dashboard/views.py
@@ -4,16 +4,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
-import controllers.summary as summary
+# import controllers.summary as summary
 import controllers.questions as questions
 
 # Create your views here.
 def dashboard(request):
     template = loader.get_template('dashboard.html')
     context = {
-        "counts": summary.get_counts(),
+        "question_count": questions.get_count(),
         "question_tags": questions.get_tags(),
-        "question_views": questions.view_count_data(),
         "question_answered": questions.is_answered_data(),
     }
     return HttpResponse(template.render(context, request))
