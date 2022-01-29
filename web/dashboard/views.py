e7f2e8a70235e616982f4dbed857aa78bbfd9664
Lesley Thuto Johnson <whosekid@gmail.com>
2022-01-29 08:07:27 +0200

    Basic view for Articles dashboard

--- web/dashboard/views.py
+++ web/dashboard/views.py
@@ -6,13 +6,22 @@ from django.template import loader
 
 # import controllers.summary as summary
 import controllers.questions as questions
+import controllers.articles as articles
 
-# Create your views here.
+# Questions dashboard
 def dashboard(request):
     template = loader.get_template('dashboard.html')
     context = {
         "question_count": questions.get_count(),
         "question_tags": questions.get_tags(),
         "question_answered": questions.is_answered_data(),
+    }
+    return HttpResponse(template.render(context, request))
+
+# Articles dashboard
+def dashboard2(request):
+    template = loader.get_template('dashboard2.html')
+    context = {
+        "articles_count": articles.get_count()
     }
     return HttpResponse(template.render(context, request))
